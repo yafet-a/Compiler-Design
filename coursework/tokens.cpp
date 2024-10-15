1e8bb118b698650dac0cf16c92a2e08448d1b64c
yafet-a <alulayafet@gmail.com>
2024-10-15 17:58:30 +0100

    rrefactored code to have lexer and tokens headers

---
+++ coursework/tokens.cpp
@@ -0,0 +1,12 @@
+#include "tokens.h"
+
+extern int lineNo, columnNo;
+
+TOKEN returnTok(std::string lexVal, int tok_type) {
+  TOKEN return_tok;
+  return_tok.lexeme = lexVal;
+  return_tok.type = tok_type;
+  return_tok.lineNo = lineNo;
+  return_tok.columnNo = columnNo - lexVal.length() - 1;
+  return return_tok;
+}
